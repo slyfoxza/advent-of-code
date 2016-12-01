0a604fb3f844c3b0247fa9b917f0eeb93f76fe23
Philip Cronje <syke107@gmail.com>
2016-12-01 18:03:42 +0200

    Implement solution for 2016.1.2

--- 2016/day1.rs
+++ 2016/day1.rs
@@ -1,8 +1,23 @@
+use std::cmp::Ordering;
+use std::collections::BTreeSet;
 use std::io::{self, Read};
 
+#[derive(Clone, Eq, Ord)]
 struct Point {
     x: i32,
     y: i32
+}
+
+impl PartialEq for Point {
+    fn eq(&self, other: &Point) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl PartialOrd for Point {
+    fn partial_cmp(&self, other: &Point) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 struct IterState {
@@ -11,8 +26,10 @@ struct IterState {
     p: Point
 }
 
-fn blocks_away(input: &str) -> i32 {
+fn blocks_away(input: &str) -> (i32, Option<i32>) {
+    let mut seen = BTreeSet::new();
     let mut state = IterState { dx: 0, dy: 1, p: Point { x: 0, y: 0 } };
+    let mut repeat = None;
     for instruction in input.split(',').map(|i| i.trim()) {
         let direction = instruction.chars().next().unwrap();
         if direction == 'L' {
@@ -33,16 +50,31 @@ fn blocks_away(input: &str) -> i32 {
             }
         }
         let distance = i32::from_str_radix(&instruction[1..], 10).unwrap();
+        for i in 0..distance {
+            let p = Point {
+                x: state.p.x + (i + 1) * state.dx,
+                y: state.p.y + (i + 1) * state.dy
+            };
+            if repeat.is_none() && !seen.insert(p.clone()) {
+                repeat = Some(p);
+            }
+        }
         state.p.x += distance * state.dx;
         state.p.y += distance * state.dy;
     }
-    state.p.x.abs() + state.p.y.abs()
+    let repeat = repeat.map(|p| p.x.abs() + p.y.abs());
+    (state.p.x.abs() + state.p.y.abs(), repeat)
 }
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    println!("{}", blocks_away(input.as_str()));
+    let (distance, first_repeat) = blocks_away(input.as_str());
+    println!("{}", distance);
+    match first_repeat {
+        Some(fr) => println!("First repeat: {}", fr),
+        None => println!("No repeat")
+    }
 }
 
 #[cfg(tests)]
@@ -51,8 +83,13 @@ mod tests {
 
     #[test]
     fn given_tests() {
-        assert_eq!(5, blocks_away("R2, L3"));
-        assert_eq!(2, blocks_away("R2, R2, R2"));
-        assert_eq!(12, blocks_away("R5, L5, R5, R3"));
+        assert_eq!((5, None), blocks_away("R2, L3"));
+        assert_eq!((2, None), blocks_away("R2, R2, R2"));
+        assert_eq!((12, None), blocks_away("R5, L5, R5, R3"));
+    }
+
+    #[test]
+    fn first_repeat_given_tests() {
+        assert_eq!((8, Some(4)), blocks_away("R8, R4, R4, R8"));
     }
 }
