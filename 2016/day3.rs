908e468d849c4d66b7eccad694482c40281f081e
Philip Cronje <syke107@gmail.com>
2016-12-03 07:51:15 +0200

    Implement solution for 2016.3.2

--- 2016/day3.rs
+++ 2016/day3.rs
@@ -1,22 +1,47 @@
 use std::io::{self, Read};
 
+fn line_to_vec(input: &str) -> Vec<u16> {
+    input.split_whitespace()
+        .map(|x| u16::from_str_radix(x, 10).unwrap())
+        .collect::<Vec<_>>()
+}
+
 fn count_possible_triangles(input: &str) -> usize {
     input.lines()
-        .map(|x| {
-            x.split_whitespace()
-                .map(|y| u16::from_str_radix(y, 10).unwrap())
-                .collect::<Vec<_>>()
-        })
+        .map(line_to_vec)
         .filter(|x| x[0] + x[1] > x[2])
         .filter(|x| x[1] + x[2] > x[0])
         .filter(|x| x[0] + x[2] > x[1])
         .count()
 }
 
+fn count_vertically(input: &str) -> usize {
+    let mut count = 0;
+    let mut rows = input.lines().map(line_to_vec);
+    loop {
+        let next = rows.next();
+        if next.is_some() {
+            let rows = [next.unwrap(), rows.next().unwrap(),
+                        rows.next().unwrap()];
+            for i in 0..3 {
+                if rows[0][i] + rows[1][i] > rows[2][i]
+                        && rows[1][i] + rows[2][i] > rows[0][i]
+                        && rows[0][i] + rows[2][i] > rows[1][i] {
+                            count += 1;
+                        }
+            }
+        } else {
+            break;
+        }
+    }
+    count
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    println!("{}", count_possible_triangles(input.as_str()));
+    println!("{} {}", count_possible_triangles(input.as_str()),
+             count_vertically(input.as_str()));
 }
 
 #[cfg(tests)]
