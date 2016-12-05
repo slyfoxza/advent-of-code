dacc64575813b6a002c279b1004a1ca37678a2ac
Philip Cronje <syke107@gmail.com>
2016-12-05 23:55:46 +0200

    Implement solution for 2016.5.2

--- 2016/day5/src/main.rs
+++ 2016/day5/src/main.rs
@@ -3,31 +3,45 @@ extern crate crypto;
 use crypto::digest::Digest;
 use crypto::md5::Md5;
 use std::io::{self, Read};
+use std::str;
 
-fn h4xx0r(door_id: &str) -> String {
+fn h4xx0r(door_id: &str) -> (String, String) {
+    static LEN: usize = 8;
     let mut password = String::new();
+    let mut password2 = Vec::with_capacity(LEN);
+    password2.resize(LEN, b'_');
+    let mut pw2filled = 0;
     let mut i = 0;
     let mut md5 = Md5::new();
-    loop {
+    while password.len() < LEN || pw2filled < LEN {
         let mut candidate = String::from(door_id);
         candidate.push_str(i.to_string().as_str());
         md5.input_str(candidate.as_str());
         let hash = md5.result_str();
         md5.reset();
         if hash.starts_with("00000") {
-            password.push(hash.chars().nth(5).unwrap());
-            if password.len() == 8 {
-                return password;
+            if password.len() < LEN {
+                password.push(hash.chars().nth(5).unwrap());
+            }
+            if pw2filled < LEN {
+                let mut bytes = hash.bytes();
+                let pos = (bytes.nth(5).unwrap() - b'0') as usize;
+                if pos < LEN && password2[pos] == b'_' {
+                    password2[pos] = bytes.next().unwrap();
+                    pw2filled += 1;
+                }
             }
         }
         i += 1;
     }
+    (password, String::from_utf8(password2).unwrap())
 }
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    println!("{}", h4xx0r(input.trim()));
+    let (password, password2) = h4xx0r(input.trim());
+    println!("{} {}", password, password2);
 }
 
 #[cfg(test)]
@@ -36,6 +50,8 @@ mod test {
     
     #[test]
     fn sample() {
-        assert_eq!("18f47a30", h4xx0r("abc"));
+        let (password, password2) = h4xx0r("abc");
+        assert_eq!("18f47a30", password);
+        assert_eq!("05ace8e3", password2);
     }
 }
