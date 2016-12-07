9d48eb2af83a5e4321faf4fbd6852424be512582
Philip Cronje <syke107@gmail.com>
2016-12-07 22:38:52 +0200

    Implement solution for 2016.7.2

--- 2016/day7.rs
+++ 2016/day7.rs
@@ -5,12 +5,11 @@ fn is_abba(s: &str) -> bool {
     assert_eq!(4, sb.len());
     let a = sb[0];
     let b = sb[1];
-    return (a != b) && (sb[2] == b) && (sb[3] == a);
+    (a != b) && (sb[2] == b) && (sb[3] == a)
 }
 
 fn has_abba(s: &str) -> bool {
-    let r = (0..s.len()-3).any(|i| is_abba(&s[i..i+4]));
-    r
+    (0..s.len()-3).any(|i| is_abba(&s[i..i+4]))
 }
 
 fn supports_tls(ip: &str) -> bool {
@@ -31,16 +30,65 @@ fn supports_tls(ip: &str) -> bool {
     }
 }
 
+fn is_aba(s: &str) -> bool {
+    let sb = s.as_bytes();
+    assert_eq!(3, sb.len());
+    let a = sb[0];
+    (a != sb[1]) && (sb[2] == a)
+}
+
+fn gather_abas<'a>(s: &'a str, abas: &mut Vec<&'a str>) {
+    for aba in (0..s.len()-2).map(|i| &s[i..i+3]).filter(|s| is_aba(s)) {
+        abas.push(aba);
+    }
+}
+
+fn find_aba_bab(abas: &Vec<&str>, babs: &Vec<&str>) -> bool {
+    if abas.is_empty() || babs.is_empty() { return false }
+    abas.iter()
+        .map(|aba| aba.as_bytes())
+        .any(|aba| {
+            babs.iter()
+                .map(|bab| bab.as_bytes())
+                .any(|bab| (aba[0] == bab[1]) && (aba[1] == bab[0]))
+        })
+}
+
+fn supports_ssl(ip: &str) -> bool {
+    let mut abas = Vec::new();
+    let mut babs = Vec::new();
+    let mut s = ip;
+    loop {
+        match s.find('[') {
+            None => {
+                abas.clear();
+                gather_abas(s, &mut abas);
+                return find_aba_bab(&abas, &babs);
+            },
+            Some(il) => {
+                gather_abas(&s[..il], &mut abas);
+                if find_aba_bab(&abas, &babs) { return true }
+                let ir = s.find(']').unwrap();
+                gather_abas(&s[il+1..ir], &mut babs);
+                if find_aba_bab(&abas, &babs) { return true }
+                s = &s[ir+1..];
+            }
+        }
+    }
+}
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
     println!("IPs supporting TLS: {}",
              input.lines().filter(|ip| supports_tls(ip)).count());
+    println!("IPs supporting SSL: {}",
+             input.lines().filter(|ip| supports_ssl(ip)).count());
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{is_abba, supports_tls};
+    use super::{is_abba, supports_ssl, supports_tls};
 
     #[test]
     fn is_abba_positive() {
@@ -63,5 +111,13 @@ mod tests {
     #[test]
     fn supports_tls_positive() {
         assert!(supports_tls("qrst[mnop]abba[ponm]tsrq"));
+    }
+
+    #[test]
+    fn supports_ssl_given() {
+        assert!(supports_ssl("aba[bab]xyz"));
+        assert!(!supports_ssl("xyx[xyx]xyx"));
+        assert!(supports_ssl("aaa[kek]eke"));
+        assert!(supports_ssl("zazbz[bzb]cdb"));
     }
 }
