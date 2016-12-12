b1256ff43a23b52a979ddad9feee9b325cafc10d
Philip Cronje <syke107@gmail.com>
2016-12-12 22:47:26 +0200

    Implement solution for 2016.12.2

--- 2016/day12.rs
+++ 2016/day12.rs
@@ -1,12 +1,10 @@
 use std::io::{self, Read};
 
-fn execute(input: &str) -> [i32; 4] {
-    let mut registers = [0; 4];
+fn execute(input: &str, init_state: [i32; 4]) -> [i32; 4] {
+    let mut registers = init_state.clone();
     let lines = input.lines().collect::<Vec<_>>();
     let mut ip: i32 = 0;
     while ip < lines.len() as i32 {
-        println!("{}: {} {} {} {}: {}", ip, registers[0], registers[1],
-                 registers[2], registers[3], lines[ip as usize]);
         match lines[ip as usize] {
             ln if ln.starts_with("inc ") => {
                 let register = (&ln[4..].as_bytes()[0] - b'a') as usize;
@@ -51,8 +49,10 @@ fn execute(input: &str) -> [i32; 4] {
 fn main() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input).unwrap();
-    let state = execute(input.as_str());
-    println!("Register `a`: {}", state[0]);
+    let state = execute(input.as_str(), [0, 0, 0, 0]);
+    println!("Register `a` (`c0` = 0): {}", state[0]);
+    let state = execute(input.as_str(), [0, 0, 1, 0]);
+    println!("Register `a` (`c0` = 1): {}", state[0]);
 }
 
 #[cfg(tests)]
@@ -61,7 +61,8 @@ mod tests {
 
     #[test]
     fn test_given() {
-        let state = execute("cpy 41 a\ninc a\ninc a\ndec a\njnz a 2\ndec a");
+        let state = execute("cpy 41 a\ninc a\ninc a\ndec a\njnz a 2\ndec a",
+                            [0, 0, 0, 0]);
         assert_eq!(42, state[0]);
     }
 }
