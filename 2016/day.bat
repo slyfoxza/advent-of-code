dd6520c8a18432cb7c8d4950052da6ef452c3013
Philip Cronje <syke107@gmail.com>
2016-12-14 00:14:21 +0200

    Support days without a text file in Windows

--- 2016/day.bat
+++ 2016/day.bat
@@ -1,1 +1,7 @@
-rustc --cfg tests --test -o test-day%1.exe day%1.rs && test-day%1.exe && rustc -O day%1.rs && day%1 < day%1.txt
+rustc --cfg tests --test -o test-day%1.exe day%1.rs && test-day%1.exe && rustc -O day%1.rs
+@IF NOT ERRORLEVEL 0 EXIT 1
+IF EXIST day%1.txt (
+    day%1 < day%1.txt
+) ELSE (
+    day%1
+)
