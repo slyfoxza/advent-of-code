094c5a21c2b56f0d4c1de95f5f2ce959c40ed411
Philip Cronje <syke107@gmail.com>
2017-12-01 08:14:52 +0200

    2017.1.2: Implement Ruby solution

--- 2017/01/test1.rb
+++ 2017/01/test1.rb
@@ -1,5 +1,15 @@
 #!/usr/bin/env ruby
+class String
+  old = instance_method(:[])
+  define_method(:[]) do |i|
+    old.bind(self).(i % length)
+  end
+end
 input = $stdin.read.strip
-accum = input[0] == input[input.length - 1] ? input[0].to_i : 0
-input.scan(/(\d)(?=\1)/) { |d| accum += d[0].to_i }
-puts accum
+accum = [0, 0]
+half = input.length / 2
+input.each_char.with_index do |c, i|
+  accum[0] += c.to_i if input[i + 1] == c
+  accum[1] += c.to_i if input[i + half] == c
+end
+puts accum.join(' ')
