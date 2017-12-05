a19f317ab634e53135e96789e25f4ebef8136a31
Philip Cronje <syke107@gmail.com>
2017-12-05 19:18:26 +0200

    2017.5: Python: Remove repeated `len` calls
    Using `cProfile` to measure runtimes before and after this change yields
    the following results:
    
    len() called in each iteration:
      30905232 function calls in 19.571 seconds
    len() called once and stored in local variable:
      12 function calls in 13.036 seconds

--- 2017/05/python.py
+++ 2017/05/python.py
@@ -3,16 +3,17 @@ from sys import stdin
 
 offsets1 = [int(e) for e in stdin.read().split()]
 offsets2 = list(offsets1)
+length = len(offsets1)
 n = [0, 0]
 
 i = 0
-while 0 <= i < len(offsets1):
+while 0 <= i < length:
     offsets1[i] += 1
     i += offsets1[i] - 1
     n[0] += 1
 
 i = 0
-while 0 <= i < len(offsets2):
+while 0 <= i < length:
     offset = offsets2[i]
     offsets2[i] += (1 if offset < 3 else -1)
     i += offset
