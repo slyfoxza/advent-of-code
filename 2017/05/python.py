c9dbe4e51261e8ed6a9502c54453f68928ad6f77
Philip Cronje <syke107@gmail.com>
2017-12-05 07:58:20 +0200

    2017.5: Implement Python solution

---
+++ 2017/05/python.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from sys import stdin
+
+offsets1 = [int(e) for e in stdin.read().split()]
+offsets2 = list(offsets1)
+n = [0, 0]
+
+i = 0
+while 0 <= i < len(offsets1):
+    offsets1[i] += 1
+    i += offsets1[i] - 1
+    n[0] += 1
+
+i = 0
+while 0 <= i < len(offsets2):
+    offset = offsets2[i]
+    offsets2[i] += (1 if offset < 3 else -1)
+    i += offset
+    n[1] += 1
+print(*n)
