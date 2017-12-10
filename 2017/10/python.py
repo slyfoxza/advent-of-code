45efabc0fc7c4d754b6b2cb47dfe0ba2b65aa03a
Philip Cronje <syke107@gmail.com>
2017-12-10 09:12:32 +0200

    2017.10.2: Implement Python solution

--- 2017/10/python.py
+++ 2017/10/python.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
+from functools import reduce
 from sys import stdin
-L = 256
-i, s, v = 0, 0, list(range(0, L))
-for x in [int(e) for e in stdin.read().strip().split(',')]:
+L, F = 256, stdin.read().strip()
+def do(i, s, x, v):
     if x == 1:
         pass
     elif i + x < L:
         v[i:i+x] = reversed(v[i:i+x])
     else:
-        r = list(reversed(v[i:] + v[:i+x-L]))
+        j = i + x - L
+        r = list(reversed(v[i:] + v[:j]))
         v[i:] = r[:L-i]
-        v[:i+x-L] = r[L-i-x:]
-    i = (i + x + s) % L
-    s += 1
-print(v[0] * v[1])
+        v[:j] = r[L-i:]
+    return (i + x + s) % L, s + 1
+def a():
+    i, s, v = 0, 0, list(range(L))
+    for x in [int(e) for e in F.split(',')]:
+        i, s = do(i, s, x, v)
+    return v[0] * v[1]
+def b():
+    i, s, v, V = 0, 0, list(range(L)), [ord(e) for e in F] + [17, 31, 73, 47, 23]
+    for n in range(64):
+        for x in V:
+            i, s = do(i, s, x, v)
+    d = [reduce(lambda x,y: x^y, v[x*16:(x+1)*16]) for x in range(16)]
+    return ''.join(format(e, '02x') for e in d)
+print(a(), b())
