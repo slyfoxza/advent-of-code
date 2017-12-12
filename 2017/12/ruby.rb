2812c25181092d0b981ef4550091594f95cde910
Philip Cronje <syke107@gmail.com>
2017-12-12 20:29:48 +0200

    2017.12.2: Implement Ruby solution

--- 2017/12/ruby.rb
+++ 2017/12/ruby.rb
@@ -1,19 +1,22 @@
 #!/usr/bin/env ruby
 require 'set'
-P = [[0]]
+P, G = [[0]], []
 $stdin.read.strip.lines.map { |x| x.strip.split(' <-> ', 2) }.each do |l|
   i = l[0].to_i
   P.fill [], P.length, i - P.length + 1 if i > P.length
   P[i] = (P[i] || []) + l[1].split(', ').map(&:to_i)
 end
-S = Set.new
-C = Array.new P[0]
-c = 0
-until C.empty?
-  n = C.pop
-  next if S.include? n
-  S << n
-  C.concat P[n]
-  c += 1
+g = 0
+(0...P.length).each do |i|
+  next if G[0, g].any? { |x| x.include? i }
+  s, c = Set.new, Array.new(P[i])
+  until c.empty?
+    n = c.pop
+    next if s.include? n
+    s << n
+    G[g] = (G[g] || []) << n
+    c.concat P[n]
+  end
+  g += 1
 end
-puts c
+puts "#{G[0].length} #{G.length}"
