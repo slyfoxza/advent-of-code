d12b0e8dfd2eaba564231277ac59f291ac48139e
Philip Cronje <syke107@gmail.com>
2017-12-22 21:54:12 +0200

    2017.22.2: Implement Python solution

--- 2017/22/python.py
+++ 2017/22/python.py
@@ -6,15 +6,39 @@ for y, line in enumerate(stdin):
     while x != -1:
         infected.add(complex(x, y))
         x = line.find('#', x + 1)
+infected_copy = set(infected)
+ibursts = [0, 0]
 pos = complex(len(line.strip()) // 2, (y + 1) // 2)
 dir = -1j
-ibursts = 0
 for n in range(10000):
     dir *= 1j if pos in infected else -1j
     if pos not in infected:
         infected.add(pos)
-        ibursts += 1
+        ibursts[0] += 1
     else:
         infected.remove(pos)
     pos += dir
+
+infected = infected_copy
+weakened = set()
+flagged = set()
+pos = complex(len(line.strip()) // 2, (y + 1) // 2)
+dir = -1j
+for n in range(10000000):
+    if pos in infected:
+        dir *= 1j
+        infected.remove(pos)
+        flagged.add(pos)
+    elif pos in flagged:
+        dir *= -1
+        flagged.remove(pos)
+    elif pos not in weakened:
+        dir *= -1j
+        weakened.add(pos)
+    else:
+        weakened.remove(pos)
+        infected.add(pos)
+        ibursts[1] += 1
+    pos += dir
+
 print(ibursts)
