108973f64f9bc4fe7d1ff3e45a27fd8c8010017b
Philip Cronje <syke107@gmail.com>
2017-12-23 10:31:27 +0200

    2017.23.2: Implement Ruby solution

--- 2017/23/ruby.rb
+++ 2017/23/ruby.rb
@@ -1,28 +1,52 @@
 #!/usr/bin/env ruby
-program = $stdin.each_line.map { |x| x.split }
-regs = {}
-n_mul = 0
-ip = 0
-until ip < 0 || ip >= program.length
-  ins, x, y = program[ip]
-  y = regs[y] if y =~ /[a-z]/
-  y = y.to_i
+$program = $stdin.each_line.map { |x| x.split }
+$regs = {'a' => 1}
+$n_mul = 0
+def run(max_ip=$program.length)
+  ip = 0
+  until ip < 0 || ip >= max_ip
+    ins, x, y = $program[ip]
+    y = $regs[y] if y =~ /[a-z]/
+    y = y.to_i
 
-  case ins
-  when 'set'; regs[x] = y
-  when 'sub'; regs[x] -= y
-  when 'mul'
-    regs[x] *= y
-    n_mul += 1
-  when 'jnz'
-    x = regs[x] if x =~ /[a-z]/
-    x = x.to_i
-    if x != 0
-      ip += y
-      next
+    case ins
+    when 'set'; $regs[x] = y
+    when 'sub'; $regs[x] -= y
+    when 'mul'
+      $regs[x] *= y
+      $n_mul += 1
+    when 'jnz'
+      x = $regs[x] if x =~ /[a-z]/
+      x = x.to_i
+      if x != 0
+        ip += y
+        next
+      end
+    end
+
+    ip += 1
+  end
+end
+run(8)
+b, c = $regs['b'], $regs['c']
+$regs = {}
+$n_mul = 0
+run
+h = 0
+(b..c).step(17).each do |b|
+  if (b & 1) == 0 || b % 3 == 0
+    h += 1
+    next
+  end
+  catch :factored do
+    i = 5
+    while i * i < b
+      if b % i == 0 || b % (i + 2) == 0
+        h += 1
+        throw :factored
+      end
+      i += 6
     end
   end
-
-  ip += 1
 end
-puts n_mul
+puts "#{$n_mul} #{h}"
