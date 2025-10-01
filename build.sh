e4385bee33ded1aac3a40b5dd6d28838b653b1e5
Philip Cronje <syke107@gmail.com>
2025-10-01 23:47:08 +0200

    Switch to Release build for C#

--- build.sh
+++ build.sh
@@ -13,6 +13,6 @@
 # You should have received a copy of the GNU General Public License along with this repository. If
 # not, see <https://www.gnu.org/licenses/>.
 set -o errexit
-dotnet build
+dotnet build --configuration=Release
 JAVA_HOME=/usr/lib/jvm/java-25-openjdk ./gradlew build
 exec ninja -C builddir
